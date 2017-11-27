21fcf12fbcf7c061985a0dbfd789f155dc8b2dbd
Elena-Anghelina <anghelinaelena96@gmail.com>
2017-11-27 17:03:50 +0200

    header

---
+++ Esential/header.js
@@ -0,0 +1,23 @@
+document.write('\
+        \
+        <nav class="fixed-nav-bar">\
+        <div class="favicon"><img src="images/fav.png"></div>\
+        <ul>\
+            <li><a href="../HomePage/index.html">Home</a></li>\
+            <li><a href="#">Features <i class="fa fa-angle-double-down" aria-hidden="true"></i></a>\
+            <ul>\
+                <li><a href="#">Charts</a></li>\
+                <li><a href="#">Infographics</a></li>\
+                <li><a href="#">Reports</a></li>\
+                <li><a href="#">Maps</a></li>\
+            </ul>\
+            </li>\
+            <li><a href="#">Blog</a></li>\
+            <li><a href="#">Forum</a></li>\
+            <li><a href="#">About</a></li>\
+            <li><a href="../Others/contact.html">Contact</a></li>\
+        </ul>\
+        <button class="button button1"><a href="../LoginPage/login.html">Log in</a></button>\
+        </nav>\
+        \
+');
