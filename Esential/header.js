93fa25bbb1682e3e88d75dfbf790d8aac2ffc0dc
Vibesx <lucian.paslariu@gmail.com>
2018-01-19 15:00:23 +0200

    added pie, doughnut and pyramid chart; implemented data reading for pie chart

--- Esential/header.js
+++ Esential/header.js
@@ -4,13 +4,7 @@ document.write('\
         <div class="favicon"><img src="../HomePage/images/fav.png"></div>\
         <ul>\
             <li><a href="../HomePage/index.html">Home</a></li>\
-            <li><a href="#">Features <i class="fa fa-angle-double-down" aria-hidden="true"></i></a>\
-            <ul>\
-                <li><a href="#">Charts</a></li>\
-                <li><a href="#">Infographics</a></li>\
-                <li><a href="#">Maps</a></li>\
-            </ul>\
-            </li>\
+            <li><a href="../Infographic/createInfographic.html">Create Infographic <i class="fa fa-angle-double-down" aria-hidden="true"></i></a></li>\
             <li><a href="../Blog/blog.html">Blog</a></li>\
             <li><a href="../Forum/forum.html">Forum</a></li>\
             <li><a href="../About/index.html">About</a></li>\
