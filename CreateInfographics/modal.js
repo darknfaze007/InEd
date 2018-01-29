4e8e97c38d5e5788b9d918d885a13bc2872b5487
Unknown <anghelinaelena96@gmail.com>
2018-01-29 13:14:27 +0200

    update modal

--- CreateInfographics/modal.js
+++ CreateInfographics/modal.js
@@ -1,23 +1,34 @@
-// Get the modal
 var modal = document.getElementById('myModal');
 
-// Get the button that opens the modal
-var btn = document.getElementById("insertPyramidChart");
+var btn1 = document.getElementById("insertChart");
+var btn2 = document.getElementById("insertDonutChart");
+var btn3 = document.getElementById("insertBarChart");
+var btn4 = document.getElementById("insertPyramidChart");
 
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal 
-btn.onclick = function() {
+btn1.onclick = function() {
     modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
+btn2.onclick = function() {
+    modal.style.display = "block";
+}
+
+
+btn3.onclick = function() {
+    modal.style.display = "block";
+}
+
+
+btn4.onclick = function() {
+    modal.style.display = "block";
+}
+
 span.onclick = function() {
     modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
